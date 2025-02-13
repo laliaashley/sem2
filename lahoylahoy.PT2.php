a5b842f04019dc78f85d9e1c3bd8f822f768bd15
laliaashley <LAHOYLAHOY.ROLINYAN@ici.edu.ph>
2025-02-14 01:45:46 +0800

    Add files via upload

---
+++ lahoylahoy.PT2.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<htm>
+<head>
+<title>Public Library Expansion Project</title>
+<style>
+body,td,th {
+  font-family: Arial, Helvetica, sans-serif;
+  color: #000000;
+  font-size: 12px;
+}
+.style1 {color: #000000}
+.style2 {
+  color: #000000;
+  font-weight: bold;
+}
+body {
+  background-color: #A3D1C6;
+}
+</style></head>
+<body>
+<table width="90%" border="1" align="center" cellpadding="0" cellspacing="0" bordercolor="#56021F">
+  <tr>
+    <td><h1 align="center">&nbsp;</h1>
+      <h1 align="center">Public Library Expansion Project</h1>
+      <h2 align="center">Cost Estimates</h2>
+      <table width="82%" border="0" align="center" cellpadding="5" cellspacing="1">
+        <tr>
+          <th width="23%" height="31" bgcolor="#7D1C4A" scope="col">Expenditures</th>
+          <th width="18%" align="right" bgcolor="#7D1C4A" scope="col">Estimated Cost</th>
+          <th width="19%" align="right" bgcolor="#7D1C4A" scope="col">10% Increase</th>
+          <th width="19%" align="right" bgcolor="#7D1C4A" scope="col">15% Increase</th>
+          <th width="21%" align="right" bgcolor="#7D1C4A" scope="col">20% Increase</th>
+        </tr>
+        <tr bgcolor="#7D1C4A">
+          <td align="center" bgcolor="#7D1C4A">Lumber</td>
+          <td align="right" bgcolor="#7D1C4A">$ 150,000.00</td>
+          <td align="right"><span class="style7">$ 165,000.00</span></td>
+          <td align="right"><span class="style7">$ 172,500.00</span></td>
+          <td align="right"><span class="style7">$ 180,000.00</span></td>
+        </tr>
+        <tr bgcolor="#7D1C4A">
+          <td align="center" bgcolor="#7D1C4A">Concrete</td>
+          <td align="right" bgcolor="#7D1C4A">$ 78,000.00</td>
+          <td align="right"><span class="style7">$ 85,800.00</span></td>
+          <td align="right"><span class="style7">$ 89,700.00</span></td>
+          <td align="right"><span class="style7">$ 93,600.00</span></td>
+        </tr>
+        <tr bgcolor="#D17D98">
+          <td align="center" bgcolor="#D17D98">Drywall</td>
+          <td align="right" bgcolor="#D17D98">$ 69,000.00</td>
+          <td align="right"><span class="style7">$ 75,900.00</span></td>
+          <td align="right"><span class="style7">$ 79,350.00</span></td>
+          <td align="right"><span class="style7">$ 82,800.00</span></td>
+        </tr>
+        <tr bgcolor="#D17D98">
+          <td align="center" bgcolor="#D17D98">Paint</td>
+          <td align="right" bgcolor="#D17D98">$ 12,000.00</td>
+          <td align="right"><span class="style7">$ 13,200.00</span></td>
+          <td align="right"><span class="style7">$ 13,800.00</span></td>
+          <td align="right"><span class="style7">$ 14,400.00</span></td>
+        </tr>
+        <tr bgcolor="#D17D98">
+          <td align="center" bgcolor="#D17D98">Miscellaneous</td>
+          <td align="right" bgcolor="#D17D98">$ 20,000.00</td>
+          <td align="right"><span class="style7">$ 22,000.00</span></td>
+          <td align="right"><span class="style7">$ 23,000.00</span></td>
+          <td align="right"><span class="style7">$ 24,000.00</span></td>
+        </tr>
+        <tr>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td align="right"><strong>Total Expenditures:</strong></td>
+          <td align="right"><span class="style2">$ 329,000.00</span></td>
+          <td align="right"><span class="style3"><span class="style2">$ 361,900.00</span></span></td>
+          <td align="right"><span class="style3"><span class="style2">$ 378,350.00</span></span></td>
+          <td align="right"><span class="style3"><span class="style2">$ 394,800.00</span></span></td>
+        </tr>
+        <tr>
+          <td align="right">&nbsp;</td>
+          <td align="right">&nbsp;</td>
+          <td align="right">&nbsp;</td>
+          <td align="right">&nbsp;</td>
+          <td align="right">&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="5" align="right"><h4>Created by: &lt;<em>Rolin Yan B. Lahoylahoy</em>&gt;</h4></td>
+        </tr>
+      </table>      
+      <p align="center" class="style1">&nbsp;</p></td>
+  </tr>
+</table>
+</body>
+</html>
